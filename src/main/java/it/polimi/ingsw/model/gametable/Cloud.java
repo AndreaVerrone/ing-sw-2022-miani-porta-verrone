5578bd21b60d8eef21d7cfd6d8695d901fdb52c7
Ciuchino <giorgio.miani@mail.polimi.it>
2022-04-06 12:56:29 +0200

    Add Cloud class

---
+++ src/main/java/it/polimi/ingsw/model/gametable/Cloud.java
@@ -0,0 +1,50 @@
+package it.polimi.ingsw.model.gametable;
+
+import it.polimi.ingsw.model.NotEnoughStudentException;
+import it.polimi.ingsw.model.PawnType;
+import it.polimi.ingsw.model.StudentList;
+
+/**
+ * Class to handle clouds
+ */
+class Cloud {
+    private final int ID;
+    private final StudentList students;
+
+    /**
+     * Class constructor
+     * @param ID cloud identification
+     */
+    public Cloud(int ID){
+        this.ID = ID;
+        this.students = new StudentList();
+    }
+
+    /**
+     * ID attribute getter
+     * @return the cloud ID
+     */
+    public int getID(){
+        return ID;
+    }
+
+    /**
+     * Adds to the cloud a student of the given {@code PawnType}
+     * @param type the type of the student to add
+     */
+    public void addStudent(PawnType type) throws NotEnoughStudentException {
+        students.changeNumOf(type, 1);
+    }
+
+    /**
+     * givesd a copy of the {@code students} attribute and empties the list by calling the {@code students.empty()} method
+     * @return the copy of the {@code students} attribute
+     */
+    public StudentList getAllStudents() {
+        StudentList studentsClone =  students.clone();
+        students.empty();
+        return studentsClone;
+    }
+
+
+}
