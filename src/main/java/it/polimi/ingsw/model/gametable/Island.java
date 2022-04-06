13221f44fe401ca8a13783ea239293e4348a17e7
AndreaVerrone <andrea.verrone@mail.polimi.it>
2022-04-06 19:00:31 +0200

    Add Island class

---
+++ src/main/java/it/polimi/ingsw/model/gametable/Island.java
@@ -0,0 +1,121 @@
+package it.polimi.ingsw.model.gametable;
+
+import it.polimi.ingsw.model.NotEnoughStudentException;
+import it.polimi.ingsw.model.PawnType;
+import it.polimi.ingsw.model.StudentList;
+import it.polimi.ingsw.model.TowerType;
+
+/**
+ * A class representing the island in the game.
+ */
+public class Island {
+
+    /**
+     * The unique ID of this island
+     */
+    private final int ID;
+
+    /**
+     * The students on this island
+     */
+    private final StudentList students = new StudentList();
+
+    /**
+     * The type of tower on this island, or null if not present
+     */
+    private TowerType tower = null;
+
+    /**
+     * The number of island tile that form this island. A value of one correspond on a single island,
+     * a value of two correspond to an island composed by two island tiles that were merged and so on.
+     */
+    private int size = 1;
+
+    /**
+     * If the island must be ignored when mother nature lands on it
+     */
+    private boolean ban = false;
+
+    /**
+     * A class representing the island in the game.
+     * @param ID the unique ID of this island
+     */
+    protected Island(int ID){
+        this.ID = ID;
+    }
+
+
+    public int getID(){
+        return ID;
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public TowerType getTower(){
+        return tower;
+    }
+
+    /**
+     * Gets the number of students of a particular type on this island.
+     * @param type the type of student to look for
+     * @return the number of students of that type
+     */
+    public int numStudentsOf(PawnType type){
+        return students.getNumOf(type);
+    }
+
+    public boolean isDisabled(){
+        return ban;
+    }
+
+    /**
+     * Replace the tower on this island with the one passed as argument
+     * @param tower the new tower to place on this island
+     */
+    public void setTower(TowerType tower){
+        assert tower != null;
+        this.tower = tower;
+    }
+
+    /**
+     *
+     * @param val {@code true} if this island must be ignored, {@code false} otherwise
+     */
+    public void setAsDisabled(boolean val){
+        ban = val;
+    }
+
+    /**
+     * Add a student of the type passed as argument on this island
+     * @param type the type of student to add
+     */
+    protected void addStudentOf(PawnType type) {
+        try {
+            students.changeNumOf(type, 1);
+        } catch (NotEnoughStudentException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Unify this island with the one passed as argument. After this call, this island will contain students
+     * from both island and will see its size increased by one. Also, if {@code island} was disabled
+     * this will also be disabled.
+     * <p>
+     * After this call, remember to remove the passed island from the game in order to not have duplicates.
+     * </p>
+     * @param island the island that need to be unified with this one.
+     */
+    protected void unifyWith(Island island){
+        assert ID != island.ID : "You can't unify the same island";
+        assert tower == island.tower : "You're trying to unify two islands that have different towers on them";
+
+        // TODO: add student list merging
+        size++;
+        if (island.isDisabled())
+            ban = true;
+    }
+
+}
