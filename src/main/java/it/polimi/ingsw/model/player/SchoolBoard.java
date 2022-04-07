ac778e5bf08bd647dace63f5bd06f09b6469fc98
AndreaVerrone <andrea.verrone@mail.polimi.it>
2022-04-07 22:32:17 +0200

    Add SchoolBoard missing documentation

--- src/main/java/it/polimi/ingsw/model/player/SchoolBoard.java
+++ src/main/java/it/polimi/ingsw/model/player/SchoolBoard.java
@@ -8,19 +8,40 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * A class representing the school board of a player.
+ * This is used to store information about the students
+ * each player has, as well as the number of towers and coins.
+ */
 class SchoolBoard {
     private final int maxNumStudentsInEntrance;
     private final int maxNumTowers;
 
+    /**
+     * The entrance of this school board. This can contain students of any type, in a maximum of
+     * 7 students in games between two or four players and 9 in games between three players.
+     */
     private final StudentList entrance = new StudentList();
+
+    /**
+     * The dining room of this school board.
+     * @see DiningRoom
+     */
     private final DiningRoom diningRoom = new DiningRoom();
     private final Set<PawnType> professorTable = new HashSet<>();
+
+    /**
+     * The number of towers remaining in this school board.
+     */
     private int towers;
+
+    /**
+     * The number of coins that the player who owns this school board has.
+     */
     private int coins = 1;
 
     /**
-     * The constructor for the school board of a player. This is used to store information about the students
-     * each player has, as well as the number of towers and coins. Based on the number of players, the initial
+     * The constructor for the school board of a player. Based on the number of players, the initial
      * number of towers and the maximum number of student in the entrance changes.
      * @param isThreePlayerGame if the game played is a match between three players or not. According to this,
      *                          the number of towers and student in entrance changes.
@@ -36,10 +57,28 @@ class SchoolBoard {
         }
     }
 
+    /**
+     * Gets the students in this school board entrance.
+     * <p>
+     * Note: this should be used only to observe the content. To modify it, use the
+     * appropriate methods of {@code SchoolBoard}.
+     * @return the students in the entrance
+     * @see #addStudentToEntrance(PawnType)
+     * @see #removeStudentFromEntrance(PawnType)
+     */
     protected StudentList getStudentsInEntrance() {
         return entrance.clone();
     }
 
+    /**
+     * Gets the professors in this school board.
+     * <p>
+     * Note: this should be used only to observe the content. To modify it, use the
+     * appropriate methods of {@code SchoolBoard}.
+     * @return the professors in this school board
+     * @see #addProfessor(PawnType)
+     * @see #removeProfessor(PawnType)
+     */
     protected Collection<PawnType> getProfessors(){
         return new HashSet<>(professorTable);
     }
@@ -61,6 +100,11 @@ class SchoolBoard {
         return coins;
     }
 
+    /**
+     * Add a student of the given type to the entrance.
+     * @param type the type of student to add
+     * @throws ReachedMaxStudentException if the entrance already has the maximum number of students
+     */
     protected void addStudentToEntrance(PawnType type) throws ReachedMaxStudentException {
         if (entrance.numAllStudents() == maxNumStudentsInEntrance) throw new ReachedMaxStudentException();
         try {
@@ -70,6 +114,11 @@ class SchoolBoard {
         }
     }
 
+    /**
+     * Remove a student of the given type from the entrance.
+     * @param type the type of student to remove
+     * @throws NotEnoughStudentException if there aren't student of the given type
+     */
     protected void removeStudentFromEntrance(PawnType type) throws NotEnoughStudentException {
         entrance.changeNumOf(type, -1);
     }
@@ -83,7 +132,7 @@ class SchoolBoard {
     }
 
     /**
-     * Add a student of a particular type in the corresponding table in the dining room. If the student
+     * Add a student of the given type in the corresponding table in the dining room. If the student
      * ends on a coin icon, this will also add a coin to the player.
      * @param type the type of student to add
      * @throws ReachedMaxStudentException if the table of that type is full
@@ -94,6 +143,11 @@ class SchoolBoard {
             coins++;
     }
 
+    /**
+     * Remove a student of the given type from the corresponding table in the dining room.
+     * @param type the type of student to remove
+     * @throws NotEnoughStudentException if the table of the given type is empty
+     */
     protected void removeStudentFromDiningRoom(PawnType type) throws NotEnoughStudentException {
         diningRoom.removeStudentOf(type);
     }
@@ -111,6 +165,14 @@ class SchoolBoard {
         coins -= cost;
     }
 
+    /**
+     * Change the number of towers in this school board by adding {@code delta} on it.
+     * Based on the sign of {@code delta} this could be an increase (if positive) or a decrease
+     * (if negative).
+     * <p>
+     * The number of towers can't exceed the maximum supported.
+     * @param delta the amount to change
+     */
     protected void changeTowerNumber(int delta){
         assert towers + delta <= maxNumTowers : "The towers added are too much";
         towers += delta;
