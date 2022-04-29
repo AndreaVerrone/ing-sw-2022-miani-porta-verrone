1d1e8045114c7b0ee4c3764f7f02b23857101795
Giorgio-Miani <giorgio.miani@mail.polimi.it>
2022-04-29 14:24:15 +0200

    Add exception classes to handle illegal arguments and operations

---
+++ src/main/java/it/polimi/ingsw/controller/NotValidArgumentException.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.controller;
+
+/**
+ * Thrown if the argument given to an operation done by the controller is invalid
+ */
+public class NotValidArgumentException extends Exception{
+}
---
+++ src/main/java/it/polimi/ingsw/controller/NotValidOperationException.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.controller;
+
+/**
+ * Thrown if an operation is called in the wrong state of the game
+ */
+public class NotValidOperationException extends Exception{
+}
