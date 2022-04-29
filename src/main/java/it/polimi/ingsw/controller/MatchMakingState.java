1aa87ade5b328ff614610c46fd857761eed213e6
AndreaVerrone <andrea.verrone@mail.polimi.it>
2022-04-29 15:14:52 +0200

    Add exceptions to the state of the match making

--- src/main/java/it/polimi/ingsw/controller/MatchMakingState.java
+++ src/main/java/it/polimi/ingsw/controller/MatchMakingState.java
@@ -11,30 +11,41 @@ public interface MatchMakingState {
     /**
      * Adds a player with the provided nickname to the lobby. The nickname must be unique.
      * @param nickname the nickname chosen by the player
+     * @throws NotValidArgumentException if the nickname is already taken
+     * @throws NotValidOperationException if a new player can't be added in the current state
      */
-    void addPlayer(String nickname);
+    void addPlayer(String nickname) throws NotValidArgumentException, NotValidOperationException;
 
     /**
      * Sets the tower type of the current player in the queue.
      * @param towerType the type of tower to assign
+     * @throws NotValidArgumentException if the tower selected is not available
+     * @throws NotValidOperationException if the tower of the player can't be changed in the current state
      */
-    void setTowerOfPlayer(TowerType towerType);
+    void setTowerOfPlayer(TowerType towerType) throws NotValidArgumentException, NotValidOperationException;
 
     /**
      * Sets the wizard of the current player in the queue.
      * @param wizard the wizard to assign
+     * @throws NotValidArgumentException if the wizard selected is not available
+     * @throws NotValidOperationException if the wizard of the player can't be changed in the current state
      */
-    void setWizardOfPlayer(Wizard wizard);
+    void setWizardOfPlayer(Wizard wizard) throws NotValidArgumentException, NotValidOperationException;
 
     /**
      * Change the number of players needed in this game. This cannot be less than the player already
      * present in the lobby.
      * @param value the new number of players
+     * @throws NotValidArgumentException if the selected number of players is not valid (i.e. if it's not
+     * one of the value supported or less than the player already present in the lobby
+     * @throws NotValidOperationException if the number of player can't be changed in the current state
      */
-    void changeNumOfPlayers(int value);
+    void changeNumOfPlayers(int value) throws NotValidArgumentException, NotValidOperationException;
 
     /**
-     * Moves the match making to the next state
+     * Moves the match making to the next state.
+     * @throws NotValidOperationException if the state can't be changed (i.e. not of the expected operations
+     * of the current state were done)
      */
-    void next();
+    void next() throws NotValidOperationException;
 }
