24c4f8147ab86454309bd832d7da60e165a43cf4
AlessiaPorta <alessia.porta@mail.polimi.it>
2022-05-02 12:27:34 +0200

    Add constructor with string to NotValidArgumentException and NotValidOperationException

--- src/main/java/it/polimi/ingsw/controller/NotValidArgumentException.java
+++ src/main/java/it/polimi/ingsw/controller/NotValidArgumentException.java
@@ -4,4 +4,10 @@ package it.polimi.ingsw.controller;
  * Thrown if the argument given to an operation done by the controller is invalid
  */
 public class NotValidArgumentException extends Exception{
+    public NotValidArgumentException(){
+        super();
+    }
+    public NotValidArgumentException(String message) {
+        super(message);
+    }
 }
--- src/main/java/it/polimi/ingsw/controller/NotValidOperationException.java
+++ src/main/java/it/polimi/ingsw/controller/NotValidOperationException.java
@@ -4,4 +4,10 @@ package it.polimi.ingsw.controller;
  * Thrown if an operation is called in the wrong state of the game
  */
 public class NotValidOperationException extends Exception{
+    public NotValidOperationException() {
+        super();
+    }
+    public NotValidOperationException(String message) {
+        super(message);
+    }
 }
