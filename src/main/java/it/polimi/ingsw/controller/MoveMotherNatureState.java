bf5652384f1c1972bea0e0314cd8b1e95ec22b91
Giorgio-Miani <giorgio.miani@mail.polimi.it>
2022-05-03 16:09:57 +0200

    Add documentation to MoveMotherNatureState

--- src/main/java/it/polimi/ingsw/controller/MoveMotherNatureState.java
+++ src/main/java/it/polimi/ingsw/controller/MoveMotherNatureState.java
@@ -6,11 +6,27 @@ import it.polimi.ingsw.model.gametable.exceptions.IslandNotFoundException;
 
 public class MoveMotherNatureState implements State {
 
+    /**
+     * Game class of the game
+     */
     private final Game game;
+    /**
+     * Model of the game
+     */
     private final GameModel model;
+    /**
+     * Game table of the game
+     */
     private final GameTable gameTable;
-    private int numberOfPlayers = 0;
+    /**
+     * Number of players that are playing the last round
+     */
+    private int numberOfPlayersLastRound = 0;
 
+    /**
+     * Constructor of the class. Saves the game, the model and the game table
+     * @param game Game class
+     */
     public MoveMotherNatureState(Game game){
         this.game = game;
         this.model = game.getModel();
@@ -34,16 +50,25 @@ public class MoveMotherNatureState implements State {
         changeState();
     }
 
+    /**
+     * Method to handle the change of the state.<p>
+     *     If it is the last round the next state will be either {@code MoveStudentState} or {@code EndState} if all
+     *     players have played their turn.
+     * </p>
+     * <p>
+     *     If it is not last round it goes to {@code ChooseCloudState}.
+     * </p>
+     */
     private void changeState(){
         if(game.getLastRoundFlag()){
             //If this is the last round
-            if (numberOfPlayers == model.getPlayerList().size()){
+            if (numberOfPlayersLastRound == model.getPlayerList().size()){
                 //If all players have played their turn go at the end of the game
                 game.setState(game.getEndState());
             }
             else{
-                //More players have to play their last turn
-                numberOfPlayers++;
+                //More players have to play their last turn, update the current player and go to the next state
+                numberOfPlayersLastRound++;
                 model.nextPlayerTurn();
                 game.setState(game.getMoveStudentState());
             }
