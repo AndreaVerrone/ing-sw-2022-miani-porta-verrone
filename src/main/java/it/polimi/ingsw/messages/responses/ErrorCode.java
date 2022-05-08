69fbcdc385d631ccade2df816b2b08f0a5e541b0
AndreaVerrone <andrea.verrone@mail.polimi.it>
2022-05-08 17:56:33 +0200

    Add a class for specifying the error code of an invalid request

---
+++ src/main/java/it/polimi/ingsw/messages/responses/ErrorCode.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw.messages.responses;
+
+/**
+ * The error code associated with an error message
+ */
+public enum ErrorCode{
+    NONE,
+    GAME_NOT_EXIST,
+    GAME_IS_FULL,
+    NICKNAME_TAKEN,
+    ASSISTANT_NOT_EXIST,
+    ASSISTANT_NOT_USABLE,
+    STUDENT_NOT_PRESENT,
+    ISLAND_NOT_EXIST,
+    DININGROOM_FULL,
+    CLOUD_EMPTY,
+    CLOUD_NOT_EXIST
+}
