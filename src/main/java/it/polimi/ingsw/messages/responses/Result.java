50cf9f84e7e748dd5b7a9206a89c0fc2679b3b66
AndreaVerrone <andrea.verrone@mail.polimi.it>
2022-05-08 17:56:04 +0200

    Add a class for specifying the result of a request

---
+++ src/main/java/it/polimi/ingsw/messages/responses/Result.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw.messages.responses;
+
+/**
+ * The result of a request
+ */
+public enum Result {
+
+        SUCCESS,
+        INVALID_ARGUMENT,
+        INVALID_OPERATION
+}
