ccff887e817dea1b1fce74f47a2dd2c01e4f526f
Giorgio-Miani <giorgio.miani@mail.polimi.it>
2022-05-09 20:27:20 +0200

    Fix typo

--- src/main/java/it/polimi/ingsw/controller/CharacterCard3.java
+++ src/main/java/it/polimi/ingsw/controller/CharacterCard3.java
@@ -8,7 +8,7 @@ public class CharacterCard3 extends CharacterCard {
 
     /**
      * Creates a new character card with the specified initial cost and the description passed as a parameter.
-     *
+     * @param game Game class of the game
      * @param cost              the initial cost of the card
      * @param effectDescription the description of the effect of this card
      */
