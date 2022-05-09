4ae086107e672c8155ffd0cd6051d0fd3ab1227f
AndreaVerrone <andrea.verrone@mail.polimi.it>
2022-05-09 21:51:19 +0200

    Add message for showing all existing games

---
+++ src/main/java/it/polimi/ingsw/messages/servertoclient/PossibleGames.java
@@ -0,0 +1,37 @@
+package it.polimi.ingsw.messages.servertoclient;
+
+import it.polimi.ingsw.client.ConnectionHandler;
+import it.polimi.ingsw.messages.responses.ResponseMessage;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+/**
+ * A message sent from server to client to send all the games that can be joined
+ */
+public class PossibleGames extends ServerCommandNetMsg{
+
+    /**
+     * All the IDs of the games present in server
+     */
+    private final HashSet<String> games;
+
+    /**
+     * Creates a new message for showing the IDs of the games passed as parameter.
+     * @param games a list of ID of games existing
+     */
+    public PossibleGames(Collection<String> games){
+        this.games = new HashSet<>(games);
+    }
+
+    @Override
+    public void processMessage(ConnectionHandler client) {
+        client.sendMessage(ResponseMessage.newSuccess(this));
+        // TODO: 09/05/2022 show in view the ids
+    }
+
+    @Override
+    public void processResponse(ResponseMessage response) {
+
+    }
+}
