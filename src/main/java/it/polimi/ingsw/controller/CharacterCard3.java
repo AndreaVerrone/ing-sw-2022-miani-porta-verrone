ba03d881a4bb906765dc9685d14ba6233fe6d58e
Giorgio-Miani <giorgio.miani@mail.polimi.it>
2022-05-09 15:46:47 +0200

    Add class for character card 3

---
+++ src/main/java/it/polimi/ingsw/controller/CharacterCard3.java
@@ -0,0 +1,25 @@
+package it.polimi.ingsw.controller;
+
+import it.polimi.ingsw.model.MotherNatureLimitPlusTwo;
+
+public class CharacterCard3 extends CharacterCard {
+
+    private final Game game;
+
+    /**
+     * Creates a new character card with the specified initial cost and the description passed as a parameter.
+     *
+     * @param cost              the initial cost of the card
+     * @param effectDescription the description of the effect of this card
+     */
+    CharacterCard3(int cost, String effectDescription, Game game) {
+        super(cost, effectDescription);
+        this.game = game;
+    }
+
+    @Override
+    public void effect() {
+        game.getModel().setMotherNatureLimitStrategy(new MotherNatureLimitPlusTwo());
+        this.effectEpilogue();
+    }
+}
