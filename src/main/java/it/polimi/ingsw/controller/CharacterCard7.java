39e08650100fce43bbc32f3a5e53be05590b4172
Giorgio-Miani <giorgio.miani@mail.polimi.it>
2022-05-09 20:21:10 +0200

    Add class for card 7

---
+++ src/main/java/it/polimi/ingsw/controller/CharacterCard7.java
@@ -0,0 +1,25 @@
+package it.polimi.ingsw.controller;
+
+import it.polimi.ingsw.model.ComputeInfluenceWithTwoAdditional;
+
+public class CharacterCard7 extends CharacterCard{
+
+    private final Game game;
+
+    /**
+     * Creates a new character card with the specified initial cost and the description passed as a parameter.
+     * @param game Game class of the game
+     * @param cost              the initial cost of the card
+     * @param effectDescription the description of the effect of this card
+     */
+    CharacterCard7(int cost, String effectDescription, Game game) {
+        super(cost, effectDescription);
+        this.game = game;
+    }
+
+    @Override
+    public void effect() {
+        game.getModel().setComputeInfluenceStrategy(new ComputeInfluenceWithTwoAdditional(game.getModel()));
+        this.effectEpilogue();
+    }
+}
