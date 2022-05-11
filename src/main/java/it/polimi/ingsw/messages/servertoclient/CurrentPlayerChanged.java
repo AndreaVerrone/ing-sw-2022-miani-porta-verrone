6c0ea8b8f90948ec7e6cabf18010681eb8e2d894
AndreaVerrone <andrea.verrone@mail.polimi.it>
2022-05-11 18:14:48 +0200

    Add a message to notify the changing of the current player

---
+++ src/main/java/it/polimi/ingsw/messages/servertoclient/CurrentPlayerChanged.java
@@ -0,0 +1,34 @@
+package it.polimi.ingsw.messages.servertoclient;
+
+import it.polimi.ingsw.client.ConnectionHandler;
+import it.polimi.ingsw.messages.responses.ResponseMessage;
+
+/**
+ * A message sent from the server to all clients connected to a game to indicate that the current player changed
+ */
+public class CurrentPlayerChanged extends ServerCommandNetMsg{
+
+    /**
+     * The nickname of the new current player
+     */
+    private final String currentPlayer;
+
+    /**
+     * Creates a new message to indicate that the current player changed.
+     * @param currentPlayer the nickname of the new current player
+     */
+    public CurrentPlayerChanged(String currentPlayer) {
+        this.currentPlayer = currentPlayer;
+    }
+
+    @Override
+    public void processMessage(ConnectionHandler client) {
+        client.sendMessage(ResponseMessage.newSuccess(this));
+        // TODO: 11/05/2022 handle change and show in view
+    }
+
+    @Override
+    public void processResponse(ResponseMessage response) {
+
+    }
+}
