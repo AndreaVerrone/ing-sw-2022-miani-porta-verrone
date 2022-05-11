0372ec119962ef20381965837eb198a8f541cafb
AndreaVerrone <andrea.verrone@mail.polimi.it>
2022-05-11 17:59:15 +0200

    Add a message to notify the movement of mother nature

---
+++ src/main/java/it/polimi/ingsw/messages/servertoclient/MotherNatureMoved.java
@@ -0,0 +1,36 @@
+package it.polimi.ingsw.messages.servertoclient;
+
+import it.polimi.ingsw.client.ConnectionHandler;
+import it.polimi.ingsw.messages.responses.ResponseMessage;
+
+/**
+ * A message sent from the server to all the client connected to a game to indicate that mother nature
+ * has moved from its previous position
+ */
+public class MotherNatureMoved extends ServerCommandNetMsg{
+
+    /**
+     * The number of movement mother nature has done
+     */
+    private final int movement;
+
+    /**
+     * Creates a new message to comunicate that mother nature has moved
+     * @param movement the number of movement
+     */
+    public MotherNatureMoved(int movement) {
+        this.movement = movement;
+    }
+
+
+    @Override
+    public void processMessage(ConnectionHandler client) {
+        client.sendMessage(ResponseMessage.newSuccess(this));
+        // TODO: 11/05/2022 update view
+    }
+
+    @Override
+    public void processResponse(ResponseMessage response) {
+
+    }
+}
