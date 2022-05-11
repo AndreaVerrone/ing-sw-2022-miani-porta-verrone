573bbd33c742a955f0db0fcaa7a10ea6163a2dad
AndreaVerrone <andrea.verrone@mail.polimi.it>
2022-05-11 19:24:01 +0200

    Change implementation of handle connection to its final form
    Also add a check for connection client side and move the one server side to make it not stop the connection

--- src/main/java/it/polimi/ingsw/client/ConnectionHandler.java
+++ src/main/java/it/polimi/ingsw/client/ConnectionHandler.java
@@ -1,10 +1,7 @@
 package it.polimi.ingsw.client;
 
 import it.polimi.ingsw.messages.NetworkMessage;
-import it.polimi.ingsw.messages.clienttoserver.*;
-import it.polimi.ingsw.messages.clienttoserver.launcher.CreateNewGame;
-import it.polimi.ingsw.messages.clienttoserver.launcher.EnterGame;
-import it.polimi.ingsw.messages.clienttoserver.launcher.GetGames;
+import it.polimi.ingsw.messages.clienttoserver.ClientCommandNetMsg;
 import it.polimi.ingsw.messages.clienttoserver.launcher.SendUserIdentifier;
 import it.polimi.ingsw.messages.responses.ResponseMessage;
 import it.polimi.ingsw.messages.servertoclient.PingMessage;
@@ -13,7 +10,11 @@ import it.polimi.ingsw.server.User;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.*;
+import java.net.SocketTimeoutException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -32,6 +33,14 @@ public class ConnectionHandler implements Runnable {
      */
     private final Map<UUID, ClientCommandNetMsg> sentMessages = new HashMap<>();
     /**
+     * The time, in seconds, after that this socket will signal a time-out exception
+     */
+    private final int SOKET_TIME_OUT = 10;
+    /**
+     * The time, in seconds, after that the socket server side will signal a timo-out exception
+     */
+    private final int Server_SOCKET_TIME_OUT = 15;
+    /**
      * The input stream from the server
      */
     private ObjectInputStream input;
@@ -39,16 +48,6 @@ public class ConnectionHandler implements Runnable {
      * The output stream to the server
      */
     private ObjectOutputStream output;
-
-    /**
-     * The time, in seconds, after that this socket will signal a time-out exception
-     */
-    private final int SOKET_TIME_OUT = 10;
-
-    /**
-     * The time, in seconds, after that the socket server side will signal a timo-out exception
-     */
-    private final int Server_SOCKET_TIME_OUT = 15;
 
 
     /**
@@ -60,7 +59,7 @@ public class ConnectionHandler implements Runnable {
      */
     protected ConnectionHandler(String serverIP, int serverPort) throws IOException {
         server = new Socket(serverIP, serverPort);
-        server.setSoTimeout(SOKET_TIME_OUT*1000);
+        server.setSoTimeout(SOKET_TIME_OUT * 1000);
     }
 
     @Override
@@ -88,7 +87,7 @@ public class ConnectionHandler implements Runnable {
         long resendDelay = 8000;
         ScheduledExecutorService executorService = Executors.newScheduledThreadPool(2);
         executorService.scheduleWithFixedDelay(
-                this::sendPing, 1, Server_SOCKET_TIME_OUT/3, TimeUnit.SECONDS);
+                this::sendPing, 1, Server_SOCKET_TIME_OUT / 3, TimeUnit.SECONDS);
         executorService.scheduleWithFixedDelay(
                 this::checkForExpired, resendDelay, resendDelay, TimeUnit.MILLISECONDS);
 
@@ -110,46 +109,15 @@ public class ConnectionHandler implements Runnable {
     }
 
     private void handleConnection() throws IOException, ClassNotFoundException {
-        // TODO: 09/05/2022 change implementation. this is used for initial debugging
-        boolean stop = false;
-        Scanner scanner = new Scanner(System.in);
-        while (!stop) {
-            System.out.println("Send a message!");
-            System.out.println("c for create, g for get all, e for enter");
-            String command = scanner.nextLine();
-            switch (command) {
-                case "c" -> {
-                    System.out.println("creating game");
-                    System.out.println("choose player number");
-                    int number = Integer.parseInt(scanner.nextLine());
-                    System.out.println("choose difficulty");
-                    boolean hard = Boolean.parseBoolean(scanner.nextLine());
-                    output.writeObject(new CreateNewGame(number, hard));
-                    ((ServerCommandNetMsg) input.readObject()).processMessage(this);
-                    stop = true;
-                }
-                case "g" -> {
-                    output.writeObject(new GetGames());
-                    ((ServerCommandNetMsg) input.readObject()).processMessage(this);
-                }
-                case "e" -> {
-                    System.out.println("choose a game");
-                    String gameID = scanner.nextLine();
-                    System.out.println("choose a nickname");
-                    String nickname = scanner.nextLine();
-                    output.writeObject(new EnterGame(nickname, gameID));
-                    var response = ((ResponseMessage) input.readObject());
-                    if (response.isSuccess())
-                        stop = true;
-                    handleMessage(response);
-                }
-            }
-        }
-
         try {
             while (true) {
-                Object message = input.readObject();
-                handleMessage(message);
+                try {
+                    Object message = input.readObject();
+                    handleMessage(message);
+                } catch (SocketTimeoutException e) {
+                    // TODO: 11/05/2022 show connection error on screen
+                    System.out.println("Connecting...");
+                }
             }
         } catch (ClassNotFoundException | ClassCastException e) {
             System.out.println("A violation of the protocol occurred");
@@ -193,10 +161,10 @@ public class ConnectionHandler implements Runnable {
         }
     }
 
-    private void sendPing(){
+    private void sendPing() {
         try {
             output.writeObject(new PingMessage());
-        } catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Can't send ping to the server");
         }
     }
@@ -221,7 +189,7 @@ public class ConnectionHandler implements Runnable {
         String identifier;
         try {
             identifier = readSecret();
-        } catch (IOException e){
+        } catch (IOException e) {
             identifier = new User().getIdentifier();
         }
 
@@ -236,7 +204,7 @@ public class ConnectionHandler implements Runnable {
         String path = "secret.txt";
         File file = new File(path);
         boolean createdNew = file.createNewFile();
-        if (createdNew){
+        if (createdNew) {
             User newUser = new User();
             DataOutputStream outputStream = new DataOutputStream(new FileOutputStream(file));
             outputStream.writeUTF(newUser.getIdentifier());
