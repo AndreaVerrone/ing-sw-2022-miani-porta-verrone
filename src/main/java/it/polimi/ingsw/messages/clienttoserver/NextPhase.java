87b9c7b541127b68e29718528afcdbf92f1acf31
AndreaVerrone <andrea.verrone@mail.polimi.it>
2022-05-11 15:45:50 +0200

    Add a message to move to the next phase of the game

---
+++ src/main/java/it/polimi/ingsw/messages/clienttoserver/NextPhase.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw.messages.clienttoserver;
+
+import it.polimi.ingsw.controller.NotValidOperationException;
+import it.polimi.ingsw.messages.responses.ResponseMessage;
+import it.polimi.ingsw.server.ClientHandler;
+
+/**
+ * A message sent from the client to the server to move to the next phase of the game
+ */
+public class NextPhase extends ClientCommandNetMsg{
+    @Override
+    protected void normalProcess(ClientHandler clientInServer) throws NotValidOperationException {
+        clientInServer.getSessionController().next();
+        clientInServer.sendMessage(ResponseMessage.newSuccess(this));
+    }
+
+    @Override
+    public void processResponse(ResponseMessage response) {
+        // TODO: 11/05/2022 handle response in client
+    }
+}
