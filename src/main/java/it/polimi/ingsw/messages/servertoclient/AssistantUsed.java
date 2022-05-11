54d48d86eab88663570de2a33fb048df4913b1dd
AndreaVerrone <andrea.verrone@mail.polimi.it>
2022-05-11 17:45:46 +0200

    Add a message to notify the use of an assistant

---
+++ src/main/java/it/polimi/ingsw/messages/servertoclient/AssistantUsed.java
@@ -0,0 +1,36 @@
+package it.polimi.ingsw.messages.servertoclient;
+
+import it.polimi.ingsw.client.ConnectionHandler;
+import it.polimi.ingsw.messages.responses.ResponseMessage;
+import it.polimi.ingsw.model.player.Assistant;
+
+/**
+ * A message sent from server to all client connected to a game to indicate that the current
+ * player used an assistant
+ */
+public class AssistantUsed extends ServerCommandNetMsg{
+
+    /**
+     * The assistant used.
+     */
+    private final Assistant assistant;
+
+    /**
+     * Creates a new message to indicate the usage of a particular assistant from the current player.
+     * @param assistant the assistant used
+     */
+    public AssistantUsed(Assistant assistant) {
+        this.assistant = assistant;
+    }
+
+    @Override
+    public void processMessage(ConnectionHandler client) {
+        client.sendMessage(ResponseMessage.newSuccess(this));
+        // TODO: 11/05/2022 update the view
+    }
+
+    @Override
+    public void processResponse(ResponseMessage response) {
+
+    }
+}
