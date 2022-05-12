4ed29105724e582fd6e684f389770dac1024ff21
Giorgio-Miani <giorgio.miani@mail.polimi.it>
2022-05-12 11:21:48 +0200

    Remove unused statement

--- src/main/java/it/polimi/ingsw/controller/EndState.java
+++ src/main/java/it/polimi/ingsw/controller/EndState.java
@@ -3,10 +3,7 @@ package it.polimi.ingsw.controller;
 import it.polimi.ingsw.model.GameModel;
 import it.polimi.ingsw.model.player.Player;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 /**
