d4c8767beeee88f6441c643bd3d0b00dea575b16
AlessiaPorta <alessia.porta@mail.polimi.it>
2022-05-13 10:43:35 +0200

    add CharacterCard5 class

---
+++ src/main/java/it/polimi/ingsw/controller/CharacterCard5.java
@@ -0,0 +1,65 @@
+package it.polimi.ingsw.controller;
+
+public class CharacterCard5 extends CharacterCard implements ObserverInterface{
+
+    /**
+     * This is the Game class
+     * @see Game
+     */
+    private final Game game;
+
+    /**
+     * the number of ban cards that are present on the island.
+     */
+    private int numOfBans;
+    /**
+     * Creates a new character card with the specified initial cost, the description
+     * and the game passed as a parameter.
+     * @param cost              the initial cost of the card
+     * @param effectDescription the description of the effect of this card
+     * @param game              the Game class
+     */
+    CharacterCard5(int cost, String effectDescription, Game game) {
+        super(cost, effectDescription);
+        this.game=game;
+        numOfBans=4;
+        // attach the subject
+        game.getModel().attach(this);
+    }
+
+    @Override
+    public void effect() {
+        game.setState(new UseCharacterCard5State(game,game.getState(),this));
+    }
+
+    public int getNumOfBans() {
+        return numOfBans;
+    }
+
+    /**
+     * This method allow to add a ban on the card.
+     * <p>
+     * Note:
+     * The number of bans on this card cannot be more that 4
+     */
+    public void addBan() {
+        assert numOfBans<=4 : "bans are too much";
+        this.numOfBans++;
+    }
+
+    /**
+     * This method allow to remove a bran from the card
+     */
+    public void removeBan() {
+        if(numOfBans<=0){
+            numOfBans=0;
+        }else {
+            this.numOfBans--;
+        }
+    }
+
+    @Override
+    public void update() {
+        addBan();
+    }
+}
