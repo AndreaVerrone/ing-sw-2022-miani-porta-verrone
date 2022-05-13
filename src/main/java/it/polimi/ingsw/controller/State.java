b5225ab0cce3a986d12f1d1ba1a5d1a6a3cc931f
AlessiaPorta <alessia.porta@mail.polimi.it>
2022-05-13 10:46:19 +0200

    add choseStudentFromLocation and chooseDestination
    these 2 methods are replacing the single custom methods

--- src/main/java/it/polimi/ingsw/controller/State.java
+++ src/main/java/it/polimi/ingsw/controller/State.java
@@ -76,11 +76,12 @@ public interface State {
     /**
      * This method allow to use the character card passed as a parameter.
      * @param characterCard the character card to use
-     * @throws NotValidArgumentException if the character card cannot be used because the player cannot pay
+     * @throws NotValidOperationException if the character card cannot be used because the player cannot pay
      * for the usage, the state of the game do not allow the usage or the player has already used a character card
      * during its turn.
+     * @throws NotValidArgumentException if the character card does not exist
      */
-    default void useCharacterCard(CharacterCard characterCard) throws NotValidOperationException{
+    default void useCharacterCard(CharacterCard characterCard) throws NotValidOperationException, NotValidArgumentException{
         characterCard.effect();
     }
 
@@ -98,7 +99,25 @@ public interface State {
         throw new NotValidOperationException();
     }
 
-    default public void chooseStudent(PawnType pawnType) throws NotValidOperationException, NotValidArgumentException{
+    /**
+     * This method allows to select a student (of the PawnType specified in the parameter) that comes from the position
+     * (also specified in the parameters).
+     * @param color the {@code PawnType} of the student
+     * @param originPosition the {@code Position} from where take the student
+     * @throws NotValidOperationException if the position is not the one that was supposed to be in the considered state
+     * @throws NotValidArgumentException if the student is not present in the specified location
+     */
+    default void choseStudentFromLocation(PawnType color, Position originPosition)throws NotValidOperationException, NotValidArgumentException{
+        throw new NotValidOperationException();
+    }
+
+    /**
+     * This method allows to choose a destination on which operate based on the state.
+     * @param destination the Position
+     * @throws NotValidOperationException if the position is not the one that was supposed to be in the considered state
+     * @throws NotValidArgumentException if the
+     */
+    default public void chooseDestination(Position destination)throws NotValidOperationException,NotValidArgumentException{
         throw new NotValidOperationException();
     }
 
