bf675e71fa6a34dab4b60d8011cb930a54de50d7
Giorgio-Miani <giorgio.miani@mail.polimi.it>
2022-05-14 16:38:45 +0200

    Add class for character card 8

--- src/main/java/it/polimi/ingsw/controller/CharacterCard8.java
+++ src/main/java/it/polimi/ingsw/controller/CharacterCard8.java
@@ -1,6 +1,14 @@
 package it.polimi.ingsw.controller;
 
+/**
+ * Class to implement card 8 to calculate the influence without counting a chosen student color
+ */
 public class CharacterCard8 extends  CharacterCard{
+    /**
+     * Game class
+     */
+    private final Game game;
+
     /**
      * Creates a new character card with the specified initial cost and the description passed as a parameter.
      * @param game Game class of the game
