6d47fa96cf4785302824d8b9ec5ffc2de6af40ab
Giorgio-Miani <giorgio.miani@mail.polimi.it>
2022-05-14 16:38:52 +0200

    Add class for character card 9

--- src/main/java/it/polimi/ingsw/controller/CharacterCard9.java
+++ src/main/java/it/polimi/ingsw/controller/CharacterCard9.java
@@ -5,10 +5,19 @@ import it.polimi.ingsw.model.PawnType;
 import it.polimi.ingsw.model.StudentList;
 import it.polimi.ingsw.model.gametable.exceptions.EmptyBagException;
 
+/**
+ * Class to implement the card 9 to swap up to three chosen students from this card with the entrance of the player
+ */
 public class CharacterCard9 extends CharacterCard{
 
+    /**
+     * Game class
+     */
     private final Game game;
 
+    /**
+     * Students on the card
+     */
     private final StudentList studentsOnCard;
     /**
      * Creates a new character card with the specified initial cost and the description passed as a parameter.
@@ -35,10 +44,19 @@ public class CharacterCard9 extends CharacterCard{
         game.setState(new UseCharacterCard9State(game, game.getState(), this));
     }
 
+    /**
+     * Take a student from a card
+     * @param pawnType student color taken from the card
+     * @throws NotEnoughStudentException if the student is not present on the card
+     */
     public void takeStudentFromCard(PawnType pawnType) throws NotEnoughStudentException {
         studentsOnCard.changeNumOf(pawnType, -1);
     }
 
+    /**
+     * Add student to the card
+     * @param pawnType student color added to the card
+     */
     public void addStudentToCard(PawnType pawnType){
         try {
             studentsOnCard.changeNumOf(pawnType, 1);
