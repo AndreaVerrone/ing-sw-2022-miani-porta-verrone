935c81f0164bef14bf33dbeb87665738548477ba
Giorgio-Miani <giorgio.miani@mail.polimi.it>
2022-05-14 16:39:30 +0200

    Add class for the state of the game for character card 8

--- src/main/java/it/polimi/ingsw/controller/UseCharacterCard8State.java
+++ src/main/java/it/polimi/ingsw/controller/UseCharacterCard8State.java
@@ -3,6 +3,9 @@ package it.polimi.ingsw.controller;
 import it.polimi.ingsw.model.ComputeInfluenceWithoutStudentColor;
 import it.polimi.ingsw.model.PawnType;
 
+/**
+ * Class to implement the state where the current player can use the card 8
+ */
 public class UseCharacterCard8State implements State{
 
     /**
@@ -20,17 +23,27 @@ public class UseCharacterCard8State implements State{
      */
     private final CharacterCard card;
 
+    /**
+     * Constructor of the class. Saves the game, the state before this one and the card used
+     * @param game game class of the game
+     * @param originState the state from which the character card has been used
+     * @param card card used in the state
+     */
     public UseCharacterCard8State(Game game, State originState, CharacterCard card){
         this.game = game;
         this.originState = originState;
         this. card = card;
     }
+
     @Override
-    public void chooseStudent(PawnType pawnType) {
-        game.getModel().setComputeInfluenceStrategy(new ComputeInfluenceWithoutStudentColor(pawnType));
-
-        card.effectEpilogue();
-
-        game.setState(originState);
+    public void choseStudentFromLocation(PawnType color, Position originPosition) throws NotValidOperationException{
+        if(originPosition.isLocation(Location.NONE)) {
+            game.getModel().setComputeInfluenceStrategy(new ComputeInfluenceWithoutStudentColor(color));
+            card.effectEpilogue();
+            game.setState(originState);
+        }
+        else{
+            throw new NotValidOperationException("You can't get from a location now!");
+        }
     }
 }
