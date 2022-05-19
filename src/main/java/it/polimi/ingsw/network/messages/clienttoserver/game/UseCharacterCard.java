c43e51d90881344b5da1800d3938df82589c5b28
Giorgio-Miani <giorgio.miani@mail.polimi.it>
2022-05-19 11:13:50 +0200

    Add messages for choose a destination, choose a student and use a character card

---
+++ src/main/java/it/polimi/ingsw/network/messages/clienttoserver/game/UseCharacterCard.java
@@ -0,0 +1,38 @@
+package it.polimi.ingsw.network.messages.clienttoserver.game;
+
+import it.polimi.ingsw.controller.CharacterCardsType;
+import it.polimi.ingsw.controller.NotValidArgumentException;
+import it.polimi.ingsw.controller.NotValidOperationException;
+import it.polimi.ingsw.network.messages.clienttoserver.ClientCommandNetMsg;
+import it.polimi.ingsw.network.messages.responses.ResponseMessage;
+import it.polimi.ingsw.server.ClientHandler;
+
+/**
+ * Message sent from the client to the server to use a character card
+ */
+public class UseCharacterCard extends ClientCommandNetMsg {
+
+    /**
+     * Character card selected
+     */
+    private final CharacterCardsType card;
+
+    /**
+     * Create a new message to communicate the character card selected
+     * @param card character card selected
+     */
+    public UseCharacterCard(CharacterCardsType card){
+        this.card = card;
+    }
+
+    @Override
+    protected void normalProcess(ClientHandler clientInServer) throws NotValidArgumentException, NotValidOperationException {
+        clientInServer.getSessionController().useCharacterCard(card);
+        clientInServer.sendMessage(ResponseMessage.newSuccess(this));
+    }
+
+    @Override
+    public void processResponse(ResponseMessage response) {
+        //TODO: 19/05/2022 handle response in client
+    }
+}
