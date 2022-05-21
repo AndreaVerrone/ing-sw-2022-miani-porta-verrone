23939dc1365b956034367057cd667989c500582f
AndreaVerrone <andrea.verrone@mail.polimi.it>
2022-05-21 13:21:26 +0200

    Add a class to internationalize the application

---
+++ src/main/java/it/polimi/ingsw/client/Translator.java
@@ -0,0 +1,78 @@
+package it.polimi.ingsw.client;
+
+import java.util.*;
+
+/**
+ * A class that give the possibility to internationalize the application
+ */
+public class Translator {
+
+    /**
+     * An enum representing the supported languages of the application
+     */
+    enum Language{
+        ITALIANO(List.of("italiano", "it", "ita", "italian")),
+        ENGLISH(List.of("english", "en", "eng", "inglese", "ing"));
+
+        /**
+         * The possible lower-case strings that correspond to this language
+         */
+        private final Collection<String> codes;
+        Language(Collection<String> codes){
+            this.codes = codes;
+        }
+
+        /**
+         * Gets a language from the specified code.
+         * If the code does not correspond to any language, it defaults to English.
+         * @param code the code of the language
+         * @return the language corresponding to the code
+         */
+        public static Language fromCode(String code){
+            if (ITALIANO.codes.contains(code.toLowerCase(Locale.ROOT)))
+                return ITALIANO;
+            return ENGLISH;
+        }
+
+        public Collection<String> getCodes(){
+            Collection<String> allCodes = new ArrayList<>();
+            for (String code : codes){
+                allCodes.add(code);
+                allCodes.add(code.toUpperCase(Locale.ROOT));
+            }
+            return Collections.unmodifiableCollection(allCodes);
+        }
+
+    }
+
+    /**
+     * If the selected language is italian.
+     */
+    private static boolean isItalian = false;
+
+    public static void setLanguage(Language language){
+        isItalian = language == Language.ITALIANO;
+    }
+
+    public static String getGameSubtitle(){
+        return isItalian ? "Un magico mondo di isole volanti!" : "A magic world of floating islands!";
+    }
+
+    public static String getChooseLanguage(){
+        return isItalian ? "Scegli una lingua" : "Choose a language";
+    }
+
+    public static String getChooseIP(){
+        return isItalian ? "Scegli l'indirizzo IP del server" : "Choose the IP address of the server";
+    }
+
+    public static String getChoosePort(){
+        return isItalian ? "Scegli la porta del server" : "Choose the port of the server";
+    }
+
+
+
+
+
+}
+
