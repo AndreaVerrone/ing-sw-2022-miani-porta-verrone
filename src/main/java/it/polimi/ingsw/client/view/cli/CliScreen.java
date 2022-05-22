09e48081d05551f99d86770fa4be33b1537b9d5f
AndreaVerrone <andrea.verrone@mail.polimi.it>
2022-05-22 18:22:54 +0200

    Add an interface for the screens of the cli

---
+++ src/main/java/it/polimi/ingsw/client/view/cli/CliScreen.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.client.view.cli;
+
+/**
+ * An interface for the screen that composes the CLI
+ */
+public interface CliScreen {
+
+    /**
+     * A method to show this screen on the command line
+     */
+    void show();
+}
