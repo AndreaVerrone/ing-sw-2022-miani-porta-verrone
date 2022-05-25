fcc5bcb66b87c688db1b34d9037857a2783d263b
Giorgio-Miani <giorgio.miani@mail.polimi.it>
2022-05-25 11:42:23 +0200

    Add class to switch between scenes

---
+++ src/main/java/it/polimi/ingsw/client/view/Switcher.java
@@ -0,0 +1,46 @@
+package it.polimi.ingsw.client.view;
+
+import it.polimi.ingsw.client.view.gui.TableView;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.PerspectiveCamera;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Translate;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Switcher {
+
+    private final Stage stage;
+
+    public Switcher(Stage stage){
+        this.stage = stage;
+    }
+
+    public void goToCreateNewGame(){
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/fxml/Table.fxml"));
+            display(root);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void display(Parent root){
+        Scene scene = new Scene(root, Color.CYAN);
+        /*
+        PerspectiveCamera camera = new PerspectiveCamera(true);
+        camera.setFieldOfView(50);
+        camera.getTransforms().addAll(new Translate(500, 900, -500), new Rotate(45, Rotate.X_AXIS));
+        camera.setNearClip(1);
+        camera.setFarClip(1000);
+        scene.setCamera(camera);
+*/
+        stage.setScene(scene);
+        stage.setFullScreen(true);
+        stage.show();
+    }
+}
