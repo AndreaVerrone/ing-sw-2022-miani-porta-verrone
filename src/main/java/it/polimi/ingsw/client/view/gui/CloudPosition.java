86f135f1d0265904ba489fb859656fd85fc50c13
Giorgio-Miani <giorgio.miani@mail.polimi.it>
2022-05-25 11:35:00 +0200

    Fix typo

--- src/main/java/it/polimi/ingsw/client/view/gui/CloudPosition.java
+++ src/main/java/it/polimi/ingsw/client/view/gui/CloudPosition.java
@@ -3,12 +3,11 @@ package it.polimi.ingsw.client.view.gui;
 public enum CloudPosition {
     CLOUD1(2,2),
     CLOUD2(3,4),
-    CLOUD3(3,2),
-    ;
+    CLOUD3(3,2);
 
     private final int column;
     private final int row;
-    
+
     CloudPosition(int column, int row) {
         this.column = column;
         this.row = row;
