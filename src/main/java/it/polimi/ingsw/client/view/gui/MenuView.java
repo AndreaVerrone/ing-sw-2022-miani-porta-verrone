9a16cdaf0fc54779f49e3792508901eab4dc7e11
Giorgio-Miani <giorgio.miani@mail.polimi.it>
2022-05-25 11:43:44 +0200

    Add controller for the fxml of the menu

---
+++ src/main/java/it/polimi/ingsw/client/view/gui/MenuView.java
@@ -0,0 +1,38 @@
+package it.polimi.ingsw.client.view.gui;
+
+import it.polimi.ingsw.client.ClientApplication;
+import it.polimi.ingsw.client.ConnectionHandler;
+import it.polimi.ingsw.client.view.Switcher;
+import it.polimi.ingsw.network.VirtualView;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class MenuView implements VirtualView {
+
+    @FXML
+    Button newGameButton;
+
+    @FXML
+    Button joinButton;
+
+    @FXML
+    Button resumeGameButton;
+
+
+    public void createNewGame(ActionEvent event){
+        ClientApplication.getSwitcher().goToCreateNewGame();
+    }
+
+    public void joinGame(ActionEvent event){
+        System.out.println("Join game");
+    }
+
+    public void resumeGame(ActionEvent event){
+        System.out.println("Resume game");
+
+    }
+
+}
