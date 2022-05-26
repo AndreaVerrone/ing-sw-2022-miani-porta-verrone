0dc5814a8ed9fd9864150739b6f693359e2610bc
Giorgio-Miani <giorgio.miani@mail.polimi.it>
2022-05-26 11:03:54 +0200

    Add methods to remove students, professors, towers

--- src/main/java/it/polimi/ingsw/client/view/gui/SchoolBoard.java
+++ src/main/java/it/polimi/ingsw/client/view/gui/SchoolBoard.java
@@ -1,16 +1,14 @@
 package it.polimi.ingsw.client.view.gui;
 
 import it.polimi.ingsw.server.controller.game.Location;
-import it.polimi.ingsw.server.controller.game.Position;
 import it.polimi.ingsw.server.model.utils.PawnType;
+import it.polimi.ingsw.server.model.utils.TowerType;
 import javafx.event.EventHandler;
-import javafx.fxml.Initializable;
 import javafx.geometry.HPos;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 
-import java.net.URL;
 import java.util.*;
 
 public class SchoolBoard {
@@ -21,61 +19,121 @@ public class SchoolBoard {
 
     private final GridPane gridTowers;
 
-    private final Map<PawnType, List<ImageView>> tables = new HashMap<>(4,1);
+    private final Map<PawnType, List<Pawn>> tables = new HashMap<>(4,1);
 
-    private final Map<PawnType, Integer> tablesPositions = new HashMap<>(4,1);
+    private final List<Pawn> professors = new ArrayList<>();
 
-    private final List<ImageView> professors = new ArrayList<>();
+    private final List<Pawn> entrance = new ArrayList<>();
 
-    private final List<ImageView> entrance = new ArrayList<>();
+    private final List<ImageView> towers = new ArrayList<>();
 
+    private final Towers towerType;
 
-    private final EventHandler<MouseEvent> handler = event -> System.out.println("SCElto");
-
-    public SchoolBoard(GridPane gridEntrance, GridPane gridDiningRoom, GridPane gridTowers){
+    public SchoolBoard(GridPane gridEntrance, GridPane gridDiningRoom, GridPane gridTowers, TowerType towerType){
+        this.towerType = Towers.typeConverter(towerType);
         this.gridEntrance = gridEntrance;
         this.gridEntrance.setOnMouseClicked(new LocationListern(Location.ENTRANCE));
         this.gridDiningRoom = gridDiningRoom;
         this.gridDiningRoom.setOnMouseClicked(new LocationListern(Location.DINING_ROOM));
         this.gridTowers = gridTowers;
         this.gridTowers.setOnMouseClicked(new LocationListern(Location.TOWER_HALL));
-        int rowCounter = 1;
         for(PawnType type : PawnType.values()) {
             tables.put(type, new ArrayList<>());
-            tablesPositions.put(type, rowCounter);
-            rowCounter ++;
         }
+        fillTowers(8);
     }
 
     public void addStudentToDiningRoom(PawnType type){
         Students studentType = Students.typeConverter(type);
         ImageView student = new ImageView(studentType.getImage());
-        tables.get(type).add(student);
+        tables.get(type).add(new Pawn(student, type));
         student.setOnMouseClicked(new StudentListener(type, Location.DINING_ROOM));
         student.toFront();
-        gridDiningRoom.add(student, tables.get(type).size(), tablesPositions.get(type));
+        gridDiningRoom.add(student, tables.get(type).size(), studentType.getTablePosition());
     }
 
     public void addStudentToEntrance(PawnType type){
         Students studentType = Students.typeConverter(type);
         ImageView student = new ImageView(studentType.getImage());
-        entrance.add(student);
+        int emptySpot = searchEmptySpotInEntrance();
+        if (emptySpot == entrance.size()){
+            entrance.add(new Pawn(student, type));
+        }
+        else{
+            entrance.set(emptySpot, new Pawn(student, type));
+        }
         student.setOnMouseClicked(new StudentListener(type, Location.ENTRANCE));
         student.toFront();
-        int row = EntrancePosition.values()[entrance.size()-1].getRow();
-        int column = EntrancePosition.values()[entrance.size()-1].getColumn();
+        int row = EntrancePosition.values()[emptySpot].getRow();
+        int column = EntrancePosition.values()[emptySpot].getColumn();
         gridEntrance.add(student, column, row);
         GridPane.setHalignment(student, HPos.RIGHT);
+    }
+
+    private int searchEmptySpotInEntrance(){
+        for(Pawn studentEmpty: entrance){
+            if(studentEmpty == null) return entrance.indexOf(studentEmpty);
+        }
+        return entrance.size();
     }
 
     public void addProfessor(PawnType type){
         Professors professorType = Professors.typeConverter(type);
         ImageView professor = new ImageView(professorType.getImage());
-        professors.add(professor);
+        professors.add(new Pawn(professor, type));
         professor.setOnMouseClicked(new StudentListener(type, Location.DINING_ROOM));
         professor.toFront();
-        gridDiningRoom.add(professor, 12, tablesPositions.get(type));
-        //professor.setRotate(90);
+        gridDiningRoom.add(professor, 12, professorType.getTablePosition());
         GridPane.setHalignment(professor, HPos.CENTER);
+    }
+
+    private void fillTowers(int numberOfTowers){
+        for(int i=0; i<numberOfTowers; i++){
+            addTower();
+        }
+    }
+
+    public void addTower(){
+        ImageView tower = new ImageView(this.towerType.getImage());
+        towers.add(tower);
+        int row = TowerPosition.values()[towers.size() - 1].getRow();
+        int column = TowerPosition.values()[towers.size() - 1].getColumn();
+        gridTowers.add(tower, column, row);
+        GridPane.setHalignment(tower, HPos.CENTER);
+    }
+
+    public void removeStudentFromDiningRoom(PawnType type){
+        ImageView studentRemoved = tables.get(type).remove(tables.get(type).size()-1).getImageView();
+        gridDiningRoom.getChildren().remove(studentRemoved);
+    }
+
+    public void removeStudentFromEntrance(PawnType type){
+        Pawn studentRemoved = null;
+        for(Pawn student : entrance){
+            if (student== null) continue;
+            if(student.getType() == type){
+                studentRemoved = student;
+                break;
+            }
+        }
+        entrance.set(entrance.indexOf(studentRemoved), null);
+        gridEntrance.getChildren().remove(studentRemoved.getImageView());
+    }
+
+    public void removeProfessor(PawnType type){
+        Pawn professorRemoved = null;
+        for(Pawn professor: professors){
+            if(professor.getType() == type){
+                professorRemoved = professor;
+                break;
+            }
+        }
+        professors.remove(professorRemoved);
+        gridDiningRoom.getChildren().remove(professorRemoved.getImageView());
+    }
+
+    public void removeTower(){
+        ImageView towerRemoved = towers.remove(towers.size()-1);
+        gridTowers.getChildren().remove(towerRemoved);
     }
 }
