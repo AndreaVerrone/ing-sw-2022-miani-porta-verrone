aaa4ac23cb4ab7f7c9c127fb71144c0b6ffa6234
AlessiaPorta <alessia.porta@mail.polimi.it>
2022-05-28 19:14:59 +0200

    add Javadoc in MoveStudentStateTest

--- src/test/java/it/polimi/ingsw/server/controller/game/states/MoveStudentStateTest.java
+++ src/test/java/it/polimi/ingsw/server/controller/game/states/MoveStudentStateTest.java
@@ -6,8 +6,11 @@ import it.polimi.ingsw.server.controller.PlayerLoginInfo;
 import it.polimi.ingsw.server.controller.game.Game;
 import it.polimi.ingsw.server.controller.game.Location;
 import it.polimi.ingsw.server.controller.game.Position;
+import it.polimi.ingsw.server.model.player.Player;
 import it.polimi.ingsw.server.model.utils.PawnType;
+import it.polimi.ingsw.server.model.utils.StudentList;
 import it.polimi.ingsw.server.model.utils.exceptions.IslandNotFoundException;
+import it.polimi.ingsw.server.model.utils.exceptions.NotEnoughStudentException;
 import it.polimi.ingsw.server.model.utils.exceptions.ReachedMaxStudentException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -37,6 +40,26 @@ class MoveStudentStateTest {
         game.setState(game.getMoveStudentState());
     }
 
+    /**
+     * This method will remove all the students from entrance of the schoolboard of the player
+     * specified in parameter
+     * @param currentPlayer the player from which remove students in entrance
+     */
+    private void removeAllStudentsFromEntrance(Player currentPlayer){
+        for(PawnType color : PawnType.values()){
+            int numOfStudents;
+            numOfStudents = currentPlayer.getStudentsInEntrance().getNumOf(color);
+            for(int i=0;i<numOfStudents;i++) {
+                try {
+                    currentPlayer.removeStudentFromEntrance(color);
+                } catch (NotEnoughStudentException e) {
+                    // not possible
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
     @AfterEach
     void tearDown() {
         game=null;
@@ -45,13 +68,22 @@ class MoveStudentStateTest {
     @Test
     public void moveStudentToIsland_BLUEUNICORN_Island1_ShouldMove(){
 
-        // add 3 BLUE UNICORN on entrance of current player
-        for (int i=0;i<3;i++){
-            try {
-                game.getModel().getCurrentPlayer().addStudentToEntrance(PawnType.BLUE_UNICORNS);
-            } catch (ReachedMaxStudentException e) {
-                fail();
-            }
+        // removeAllStudentsFromEntrance all students from entrance to start from a clean situation
+        removeAllStudentsFromEntrance(game.getModel().getCurrentPlayer());
+
+        // add 1 BLUE UNICORN on entrance of current player
+        try {
+            game.getModel().getCurrentPlayer().addStudentToEntrance(PawnType.BLUE_UNICORNS);
+        } catch (ReachedMaxStudentException e) {
+            fail();
+        }
+
+        // the num of BLUE UNICORN on island 1 before the calling of the method to test
+        int oldNumOfBlueUnicornOnIsland1 = 0;
+        try {
+            oldNumOfBlueUnicornOnIsland1 = game.getModel().getGameTable().getIsland(1).numStudentsOf(PawnType.BLUE_UNICORNS);
+        } catch (IslandNotFoundException e) {
+            fail();
         }
 
         // set island
@@ -71,11 +103,12 @@ class MoveStudentStateTest {
         }
 
         // CHECKS
-        // 1. there are 2 student in the entrance of the current player (i.e., one was removed)
-        assertEquals(2,game.getModel().getCurrentPlayer().getStudentsInEntrance().getNumOf(PawnType.BLUE_UNICORNS));
-        // 2. check that there is 1 blue student on island
+        // 1. the num of BLUE UNICORNS at entrance has decreased by 1 (i.e., one was removed)
+        assertEquals(0,game.getModel().getCurrentPlayer().getStudentsInEntrance().getNumOf(PawnType.BLUE_UNICORNS));
+
+        // 2. check that the num of BLUE UNICORN on island has been increased by 1
         try {
-            assertEquals(1,game.getModel().getGameTable().getIsland(1).numStudentsOf(PawnType.BLUE_UNICORNS));
+            assertEquals(oldNumOfBlueUnicornOnIsland1+1,game.getModel().getGameTable().getIsland(1).numStudentsOf(PawnType.BLUE_UNICORNS));
         } catch (IslandNotFoundException e) {
             fail();
         }
@@ -84,13 +117,14 @@ class MoveStudentStateTest {
     @Test
     public void moveStudentToIsland_BLUEUNICORN_NotExistingIsland_ShouldThrowAndDoNothing(){
 
-        // add 3 BLUE UNICORN on entrance of current player
-        for (int i=0;i<3;i++){
-            try {
-                game.getModel().getCurrentPlayer().addStudentToEntrance(PawnType.BLUE_UNICORNS);
-            } catch (ReachedMaxStudentException e) {
-                fail();
-            }
+        // removeAllStudentsFromEntrance all students from entrance to start from a clean situation
+        removeAllStudentsFromEntrance(game.getModel().getCurrentPlayer());
+
+        // add 1 BLUE UNICORN on entrance of current player
+        try {
+            game.getModel().getCurrentPlayer().addStudentToEntrance(PawnType.BLUE_UNICORNS);
+        } catch (ReachedMaxStudentException e) {
+            fail();
         }
 
         // set island
@@ -109,12 +143,22 @@ class MoveStudentStateTest {
                 NotValidArgumentException.class,
                 ()->game.getMoveStudentState().chooseDestination(island100)
         );
-        // 2. there are still 3 BLUE UNICORN in the entrance of the current player
-        assertEquals(3,game.getModel().getCurrentPlayer().getStudentsInEntrance().getNumOf(PawnType.BLUE_UNICORNS));
+        // 2. there are still the same num of BLUE UNICORN in the entrance of the current player
+        assertEquals(1,game.getModel().getCurrentPlayer().getStudentsInEntrance().getNumOf(PawnType.BLUE_UNICORNS));
     }
 
     @Test
     public void moveStudentToIsland_BLUEUNICORN_NotPresentStudent_ShouldThrowAndDoNothing(){
+
+        // removeAllStudentsFromEntrance all BLUE UNICORN from entrance if present
+        int numOfBlue = game.getModel().getCurrentPlayer().getStudentsInEntrance().getNumOf(PawnType.BLUE_UNICORNS);
+        for(int i=0;i<numOfBlue;i++) {
+            try {
+                game.getModel().getCurrentPlayer().removeStudentFromEntrance(PawnType.BLUE_UNICORNS);
+            } catch (NotEnoughStudentException e) {
+                throw new RuntimeException(e);
+            }
+        }
 
         // CHECKS
         // 1. NotValidOperationException has been thrown
@@ -133,6 +177,9 @@ class MoveStudentStateTest {
 
     @Test
     public void moveStudentToDiningRoom_BLUEUNICORN_DiningRoomFull_ShouldThrow(){
+
+        // removeAllStudentsFromEntrance all students from entrance to start from a clean situation
+        removeAllStudentsFromEntrance(game.getModel().getCurrentPlayer());
 
         // add 1 BLUE UNICORN at the entrance
         try {
@@ -165,12 +212,18 @@ class MoveStudentStateTest {
     @Test
     public void moveStudentToDiningRoom_BLUEUNICORN_NoErrors_ShouldMove(){
 
+        // removeAllStudentsFromEntrance all students from entrance to start from a clean situation
+        removeAllStudentsFromEntrance(game.getModel().getCurrentPlayer());
+
         // add 1 student to entrance
         try {
             game.getModel().getCurrentPlayer().addStudentToEntrance(PawnType.BLUE_UNICORNS);
         } catch (ReachedMaxStudentException e) {
             fail();
         }
+
+        // compute num of BLUE UNICORN at the entrance
+        int oldNumOfBlueUnicorns = game.getModel().getCurrentPlayer().getStudentsInEntrance().getNumOf(PawnType.BLUE_UNICORNS);
 
         // apply the method to test
         try {
@@ -184,8 +237,8 @@ class MoveStudentStateTest {
             fail();
         }
 
-        // 1. there aren't BLUE UNICORN at the entrance
-        assertEquals(0,game.getModel().getCurrentPlayer().getStudentsInEntrance().getNumOf(PawnType.BLUE_UNICORNS));
+        // 1. num of BLUE UNICORN at the entrance has decreased by 1
+        assertEquals(oldNumOfBlueUnicorns-1,game.getModel().getCurrentPlayer().getStudentsInEntrance().getNumOf(PawnType.BLUE_UNICORNS));
         // 2. there is one BLUE UNICORN in the dining room
         assertEquals(1,game.getModel().getCurrentPlayer().getNumStudentOf(PawnType.BLUE_UNICORNS));
 
@@ -193,6 +246,9 @@ class MoveStudentStateTest {
 
     @Test
     public void MoveStudentToIsland_called4Times_ShouldChangeState(){
+
+        // removeAllStudentsFromEntrance all students from entrance to start from a clean situation
+        removeAllStudentsFromEntrance(game.getModel().getCurrentPlayer());
 
         // add 4 BLUE UNICORN on entrance of current player
         for (int i=0;i<4;i++){
@@ -224,6 +280,9 @@ class MoveStudentStateTest {
     @Test
     public void MoveStudentToIsland_called1Time_ShouldNotChangeState(){
 
+        // removeAllStudentsFromEntrance all students from entrance to start from a clean situation
+        removeAllStudentsFromEntrance(game.getModel().getCurrentPlayer());
+
         // add 4 BLUE UNICORN on entrance of current player
         for (int i=0;i<4;i++){
             try {
@@ -252,6 +311,9 @@ class MoveStudentStateTest {
     @Test
     public void MoveStudentToDiningRoom_called4Times_ShouldChangeState(){
 
+        // removeAllStudentsFromEntrance all students from entrance to start from a clean situation
+        removeAllStudentsFromEntrance(game.getModel().getCurrentPlayer());
+
         // add 4 BLUE UNICORN on entrance of current player
         for (int i=0;i<4;i++){
             try {
@@ -278,14 +340,17 @@ class MoveStudentStateTest {
     @Test
     public void MoveStudentToDiningRoom_called1Time_ShouldNotChangeState(){
 
-        // add 4 BLUE UNICORN on entrance of current player
-        for (int i=0;i<4;i++){
-            try {
-                game.getModel().getCurrentPlayer().addStudentToEntrance(PawnType.BLUE_UNICORNS);
-            } catch (ReachedMaxStudentException e) {
-                fail();
-            }
+        // removeAllStudentsFromEntrance all students from entrance to start from a clean situation
+        removeAllStudentsFromEntrance(game.getModel().getCurrentPlayer());
+
+        // add 1 BLUE UNICORN on entrance of current player
+
+        try {
+            game.getModel().getCurrentPlayer().addStudentToEntrance(PawnType.BLUE_UNICORNS);
+        } catch (ReachedMaxStudentException e) {
+            fail();
         }
+
 
         // move 1 student to dining room
         try {
@@ -302,6 +367,9 @@ class MoveStudentStateTest {
 
     @Test
     public void Move4SomeToIslandAndSomeOtherInDiningRoom_ShouldChangeState() {
+
+        // removeAllStudentsFromEntrance all students from entrance to start from a clean situation
+        removeAllStudentsFromEntrance(game.getModel().getCurrentPlayer());
 
         // set island
         Position island1 = new Position(Location.ISLAND);
