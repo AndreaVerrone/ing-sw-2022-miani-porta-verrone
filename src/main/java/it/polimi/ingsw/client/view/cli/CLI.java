05aa721a84f6029e023e94cd929f0a0df67d04b3
Giorgio-Miani <giorgio.miani@mail.polimi.it>
2022-06-06 13:07:00 +0200

    Add methods to CLI class to avoid error

--- src/main/java/it/polimi/ingsw/client/view/cli/CLI.java
+++ src/main/java/it/polimi/ingsw/client/view/cli/CLI.java
@@ -7,10 +7,19 @@ import it.polimi.ingsw.client.view.cli.fancy_cli.inputs.Validator;
 import it.polimi.ingsw.client.view.cli.fancy_cli.utils.Color;
 import it.polimi.ingsw.client.view.cli.fancy_cli.widgets.Canvas;
 import it.polimi.ingsw.network.VirtualView;
+import it.polimi.ingsw.server.controller.PlayerLoginInfo;
+import it.polimi.ingsw.server.controller.StateType;
+import it.polimi.ingsw.server.controller.game.expert.CharacterCardsType;
+import it.polimi.ingsw.server.model.player.Assistant;
+import it.polimi.ingsw.server.model.player.Wizard;
+import it.polimi.ingsw.server.model.utils.PawnType;
+import it.polimi.ingsw.server.model.utils.StudentList;
+import it.polimi.ingsw.server.model.utils.TowerType;
 import org.jline.reader.impl.completer.AggregateCompleter;
 import org.jline.reader.impl.completer.EnumCompleter;
 import org.jline.reader.impl.completer.StringsCompleter;
 
+import java.util.Collection;
 import java.util.Locale;
 
 /**
@@ -122,5 +131,135 @@ public class CLI implements VirtualView, Runnable {
             case "y", "yes", "s", "si" -> true;
             default -> false;
         };
+    }
+
+    @Override
+    public void changeCurrentState(StateType stateType) {
+
+    }
+
+    @Override
+    public void addCoinOnCard(CharacterCardsType characterCardsType, boolean coinOnCard) {
+
+    }
+
+    @Override
+    public void addStudentsOnCard(CharacterCardsType characterCardType, StudentList actualStudents) {
+
+    }
+
+    @Override
+    public void changeNumberOfPlayers(int numberOfPlayers) {
+
+    }
+
+    @Override
+    public void playersChanged(Collection<PlayerLoginInfo> players) {
+
+    }
+
+    @Override
+    public void towerSelected(String player, TowerType tower) {
+
+    }
+
+    @Override
+    public void wizardSelected(String player, Wizard wizard) {
+
+    }
+
+    @Override
+    public void changeNumberOfBansOnIsland(int islandIDWithBan, int actualNumOfBans) {
+
+    }
+
+    @Override
+    public void changeAssistantDeck(String nickName, Collection<Assistant> actualDeck) {
+
+    }
+
+    @Override
+    public void changeCoinNumberInBag(int actualNumOfCoins) {
+
+    }
+
+    @Override
+    public void changeCoinNumber(String nickNameOfPlayer, int actualNumOfCoins) {
+
+    }
+
+    @Override
+    public void changeCurrentPlayer(String actualCurrentPlayerNickname) {
+
+    }
+
+    @Override
+    public void changeTowerNumber(String nickName, int numOfActualTowers) {
+
+    }
+
+    @Override
+    public void conquerIslandObserver() {
+
+    }
+
+    @Override
+    public void emptyStudentBag() {
+
+    }
+
+    @Override
+    public void islandNumberChanged(int actualNumOfIslands) {
+
+    }
+
+    @Override
+    public void islandUnification(int islandID, int islandRemovedID, int finalSize) {
+
+    }
+
+    @Override
+    public void changeLastAssistantUsed(String nickName, Assistant actualLastAssistant) {
+
+    }
+
+    @Override
+    public void changeMotherNaturePosition(int actualMotherNaturePosition) {
+
+    }
+
+    @Override
+    public void changeProfessor(String nickName, Collection<PawnType> actualProfessors) {
+
+    }
+
+    @Override
+    public void changeStudentsInDiningRoom(String nickname, StudentList actualStudents) {
+
+    }
+
+    @Override
+    public void changeStudentsOnCloud(int cloudID, StudentList actualStudentList) {
+
+    }
+
+    @Override
+    public void changeStudentsOnEntrance(String nickname, StudentList actualStudents) {
+
+    }
+
+    @Override
+    public void changeStudentsOnIsland(int islandID, StudentList actualStudents) {
+
+    }
+
+    @Override
+    public void changeTowerOnIsland(int islandIDWithChange, TowerType actualTower) {
+
+    }
+
+    @Override
+    public void endGame(Collection<String> winners) {
+
     }
 }
