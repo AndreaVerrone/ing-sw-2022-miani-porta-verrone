a3e0fbadfd08da0b5fba4b1c88291b2a73f2b77e
AlessiaPorta <alessia.porta@mail.polimi.it>
2022-06-10 19:38:57 +0200

    use Pawn Icon in professor table

--- src/main/java/it/polimi/ingsw/client/view/cli/game/custom_widgets/schoolboard/ProfTable.java
+++ src/main/java/it/polimi/ingsw/client/view/cli/game/custom_widgets/schoolboard/ProfTable.java
@@ -3,6 +3,7 @@ package it.polimi.ingsw.client.view.cli.game.custom_widgets.schoolboard;
 import it.polimi.ingsw.client.Translator;
 import it.polimi.ingsw.client.view.cli.fancy_cli.utils.BorderType;
 import it.polimi.ingsw.client.view.cli.fancy_cli.utils.Color;
+import it.polimi.ingsw.client.view.cli.fancy_cli.utils.Icons;
 import it.polimi.ingsw.client.view.cli.fancy_cli.utils.TextStyle;
 import it.polimi.ingsw.client.view.cli.fancy_cli.widgets.*;
 import it.polimi.ingsw.server.model.utils.PawnType;
@@ -44,42 +45,40 @@ class ProfTable extends StatefulWidget {
     @Override
     protected Widget build() {
 
+        // the name of the widget
         String stringHeader = Translator.getProfTableHeader();
         Text header = new Text(stringHeader).addTextStyle(TextStyle.ITALIC).addTextStyle(TextStyle.BOLD);
 
+        // colum of professors
         Text blueStudents = new Text("").setForegroundColor(Color.BLUE);
         Text greenStudents = new Text("").setForegroundColor(Color.GREEN);
         Text yellowStudents = new Text("").setForegroundColor(Color.BRIGHT_YELLOW);
         Text redStudents = new Text("").setForegroundColor(Color.RED);
         Text pinkStudents = new Text("").setForegroundColor(Color.MAGENTA);
 
+        // centering of the icons of professors
         int numOfSpace =  stringHeader.length()/2;
         StringBuilder padding = new StringBuilder();
         padding.append(" ".repeat(numOfSpace));
 
         if(actualProfessors.contains(PawnType.BLUE_UNICORNS)) {
-            blueStudents.setText(padding + "█");
-            //blueStudents.setText(padding + "\uD83D\uDFE6");
+            blueStudents.setText(padding + String.valueOf(Icons.PAWN));
         }
 
         if(actualProfessors.contains(PawnType.GREEN_FROGS)) {
-            greenStudents.setText(padding + "█");
-            //greenStudents.setText(padding+ "\uD83D\uDFE9");
+            greenStudents.setText(padding + String.valueOf(Icons.PAWN));
         }
 
         if(actualProfessors.contains(PawnType.YELLOW_GNOMES)) {
-            yellowStudents.setText(padding + "█");
-            //yellowStudents.setText(padding+"\uD83D\uDFE8");
+            yellowStudents.setText(padding + String.valueOf(Icons.PAWN));
         }
 
         if(actualProfessors.contains(PawnType.RED_DRAGONS)) {
-            redStudents.setText(padding + "█");
-            //redStudents.setText(padding+"\uD83D\uDFE5");
+            redStudents.setText(padding + String.valueOf(Icons.PAWN));
         }
 
         if(actualProfessors.contains(PawnType.PINK_FAIRIES)) {
-            pinkStudents.setText(padding + "█");
-            //pinkStudents.setText(padding + "\uD83D\uDFEA");
+            pinkStudents.setText(padding + String.valueOf(Icons.PAWN));
         }
 
         return new Border(
