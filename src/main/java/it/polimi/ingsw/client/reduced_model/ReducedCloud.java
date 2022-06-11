bb99a8adc38796d32759b8589bf80fbbc84866e5
AlessiaPorta <alessia.porta@mail.polimi.it>
2022-06-11 12:10:13 +0200

    add missing javaDoc

--- src/main/java/it/polimi/ingsw/client/reduced_model/ReducedCloud.java
+++ src/main/java/it/polimi/ingsw/client/reduced_model/ReducedCloud.java
@@ -1,5 +1,11 @@
 package it.polimi.ingsw.client.reduced_model;
 
 import it.polimi.ingsw.server.model.utils.StudentList;
+
+/**
+ * A simplified version of a cloud, used to store and use the information client side
+ * @param ID the unique ID of the cloud
+ * @param students the students on the cloud
+ */
 public record ReducedCloud(int ID, StudentList students) {
 }
