2019f852dae01464c78820e848e2383bedc1cb07
AlessiaPorta <alessia.porta@mail.polimi.it>
2022-06-11 17:21:35 +0200

    change order in definition of the enum

--- src/main/java/it/polimi/ingsw/server/model/utils/PawnType.java
+++ src/main/java/it/polimi/ingsw/server/model/utils/PawnType.java
@@ -4,9 +4,9 @@ package it.polimi.ingsw.server.model.utils;
  * A class representing the types of students and professors
  */
 public enum PawnType {
-    YELLOW_GNOMES,
     BLUE_UNICORNS,
     GREEN_FROGS,
+    YELLOW_GNOMES,
     RED_DRAGONS,
     PINK_FAIRIES
 }
