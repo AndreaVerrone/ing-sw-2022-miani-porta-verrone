03952d98c1b9cf569918a3ca1ec1c6eca6d96452
AlessiaPorta <alessia.porta@mail.polimi.it>
2022-06-17 21:54:09 +0200

    fix centering and position of items

--- src/main/java/it/polimi/ingsw/client/view/gui/controller/ChooseLanguageScreen.java
+++ src/main/java/it/polimi/ingsw/client/view/gui/controller/ChooseLanguageScreen.java
@@ -1,27 +1,34 @@
 package it.polimi.ingsw.client.view.gui.controller;
 
 import it.polimi.ingsw.client.ClientApplication;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.RadioButton;
 
-import java.util.List;
-
+/**
+ * This class is the controller of the screen used to make the user choose the language
+ * of the game.
+ */
 public class ChooseLanguageScreen {
 
     @FXML
     private RadioButton itaButton, engButton;
 
-    public void getLanguage(ActionEvent actionEvent) {
+    /**
+     * this method is called when the {@code okButton} button is pressed.
+     * It will set the language, and it will allow to pass to the screen to choose the server.
+     */
+    public void setLanguageAndChangeScreen() {
 
+        // set the language
         if(itaButton.isSelected()){
             // Translator.setLanguage(Language.ITALIANO) // todo: actual code
-            System.out.println("ITALIAN SELECTED");
+            System.out.println("ITALIAN SELECTED"); // todo: testing code
         }else if(engButton.isSelected()){
             // Translator.setLanguage(Language.ENGLISH) // todo: actual code
-            System.out.println("ENGLISH SELECTED");
+            System.out.println("ENGLISH SELECTED"); // todo: testing code
         }
 
+        // go to screen to choose the server.
         ClientApplication.getSwitcher().goToAskServerParameters();
     }
 }
