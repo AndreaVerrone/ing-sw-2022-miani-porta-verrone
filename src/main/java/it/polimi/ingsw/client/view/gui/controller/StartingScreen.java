e2a0b48f5630864a5be0f2047a080c2bab411b9e
AlessiaPorta <alessia.porta@mail.polimi.it>
2022-06-17 20:26:34 +0200

    add javadoc

--- src/main/java/it/polimi/ingsw/client/view/gui/controller/StartingScreen.java
+++ src/main/java/it/polimi/ingsw/client/view/gui/controller/StartingScreen.java
@@ -8,14 +8,22 @@ import javafx.fxml.FXML;
 
 import java.util.List;
 
+/**
+ * This class is the controller of the starting screen of the game
+ */
 public class StartingScreen {
+
+    /**
+     * This method is called when the {@code startButton} is pressed.
+     * It allows to go to the screen to choose the language.
+     */
     public void startGame(){
-        ClientApplication.getSwitcher().goToChooseWizardAndTower(
+        ClientApplication.getSwitcher().goToChooseLanguageScreen();
+        /*ClientApplication.getSwitcher().goToChooseWizardAndTower(
                 List.of(Wizard.W1,Wizard.W2,Wizard.W3,Wizard.W4),
                 List.of(TowerType.WHITE,TowerType.GREY,TowerType.BLACK)
-        );
+        );*
         //ClientApplication.getSwitcher().goToChooseGameScreen(List.of("game 1", "game 2", "game 3"));
-        //ClientApplication.getSwitcher().goToChooseWizardAndTower();
-        //ClientApplication.getSwitcher().goToChooseLanguageScreen();
+        //ClientApplication.getSwitcher().goToChooseWizardAndTower();*/
     }
 }
