e437b9794f88e395144be884182f97a6b6608381
AlessiaPorta <alessia.porta@mail.polimi.it>
2022-06-18 16:49:07 +0200

    add missing javadoc

--- src/main/java/it/polimi/ingsw/client/view/gui/controller/MenuView.java
+++ src/main/java/it/polimi/ingsw/client/view/gui/controller/MenuView.java
@@ -6,37 +6,83 @@ import it.polimi.ingsw.client.view.Switcher;
 import it.polimi.ingsw.network.VirtualView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+import java.net.URL;
 import java.util.List;
+import java.util.ResourceBundle;
 
-public class MenuView implements VirtualView {
+public class MenuView implements Initializable{
 
+    /**
+     * button to create a new game
+     */
     @FXML
     Button newGameButton;
 
+    /**
+     * button to join an existing game
+     */
     @FXML
     Button joinButton;
 
+    /**
+     * button to resume a game
+     */
     @FXML
     Button resumeGameButton;
 
-
-    public void createNewGame(ActionEvent event){
-        ClientApplication.getSwitcher().goToCreateNewGame();
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        buttonsSetUp();
     }
 
-    public void joinGame(ActionEvent event){
-        System.out.println("Join game");
+    /**
+     * This method is called when {@code newGameButton} is pressed.
+     * It allows to pass to the screen to create the game (i.e., choose
+     * number of players and difficulty)
+     */
+    public void createNewGame(){
+        ClientApplication.getSwitcher().goToCreateGameScreen();
     }
 
-    public void resumeGame(ActionEvent event){
+    /**
+     * This method is called when {@code joinButton} is pressed.
+     * It will allow to join an existing game.
+     */
+    public void joinGame(){
+        // It will ask the server the list of the available games.
+        // In the meantime it will display idle screen.
+        // todo: this is only for testing
+        ClientApplication.getSwitcher().goToChooseGameScreen(
+                List.of("Game 1," + "Game 2", "Game 3", "Game 4")
+        );
+        // todo: actual code
+        // ask available games to server
+        // getClientController().getGames();
+        // display idle screen
+    }
+
+    /**
+     * This method is called when the {@code resumeButton} is pressed.
+     * It will allow to resume a game.
+     */
+    public void resumeGame(){
         ClientApplication.getSwitcher().goToCreateGameScreen();
         // ClientApplication.getSwitcher().goToExitScreen(List.of("Player 1"));
         //System.out.println("Resume game");
-
     }
 
+    /**
+     * This method is used to set up the text of the buttons.
+     */
+    private void buttonsSetUp(){
+        // todo: add translation
+        newGameButton.setText("NEW GAME");
+        joinButton.setText("JOIN");
+        resumeGameButton.setText("RESUME");
+    }
 }
