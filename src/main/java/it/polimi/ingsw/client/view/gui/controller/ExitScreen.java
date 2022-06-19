a76c4a03cdc09744f8235e8f86399a694ed874fc
AlessiaPorta <alessia.porta@mail.polimi.it>
2022-06-19 15:52:23 +0200

    fix centering

--- src/main/java/it/polimi/ingsw/client/view/gui/controller/ExitScreen.java
+++ src/main/java/it/polimi/ingsw/client/view/gui/controller/ExitScreen.java
@@ -1,10 +1,7 @@
 package it.polimi.ingsw.client.view.gui.controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -56,6 +53,19 @@ public class ExitScreen {
         alert.setTitle("Exit from game");
         alert.setHeaderText("You're about to exit from game");
         alert.setContentText("Do you want to exit the game ?");
+
+        // Create the ButtonBar instance
+        ButtonBar buttonBar = new ButtonBar();
+
+        // Create the buttons to go into the ButtonBar
+        Button yesButton = new Button("Yes");
+        ButtonBar.setButtonData(yesButton, ButtonBar.ButtonData.YES);
+
+        Button noButton = new Button("No");
+        ButtonBar.setButtonData(noButton, ButtonBar.ButtonData.NO);
+
+        // Add buttons to the ButtonBar
+        buttonBar.getButtons().addAll(yesButton, noButton);
 
         if(alert.showAndWait().get() == ButtonType.OK) {
             // todo: maybe use also the proper method of the controller to exit the game !
