a75890d35dc4bb299d548c329ba34c3a4389dae7
AlessiaPorta <alessia.porta@mail.polimi.it>
2022-06-23 18:04:43 +0200

    remove useless code and add javadoc to ClientGuiStart class

--- src/main/java/it/polimi/ingsw/client/view/gui/ClientGuiStart.java
+++ src/main/java/it/polimi/ingsw/client/view/gui/ClientGuiStart.java
@@ -4,7 +4,6 @@ package it.polimi.ingsw.client.view.gui;
  * This class is used only to run the gui in intellij
  */
 public class ClientGuiStart {
-    @SuppressWarnings("MissingJavadoc")
     public static void main(String[] args) {
         ClientGui.main(args);
     }
