d9339625b07e58ea46ac7ce701de59450add8578
AlessiaPorta <alessia.porta@mail.polimi.it>
2022-06-23 18:03:19 +0200

    remove useless code and add javadoc to ClientGuiStart class

--- src/main/java/it/polimi/ingsw/client/view/gui/ClientGuiStart.java
+++ src/main/java/it/polimi/ingsw/client/view/gui/ClientGuiStart.java
@@ -1,32 +1,13 @@
 package it.polimi.ingsw.client.view.gui;
 
+/**
+ * This class is used only to run the gui in intellij
+ */
 public class ClientGuiStart {
-
-        public static void main(String[] args) {
-            ClientGui.main(args);
-        }
+    @SuppressWarnings("MissingJavadoc")
+    public static void main(String[] args) {
+        ClientGui.main(args);
+    }
 }
-
-
-/*
-
-        // TODO: 09/05/2022 initialize view and ask for server parameters
-
-        //code used for initial debugging
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Server port?");
-        int socketPort = Integer.parseInt(scanner.nextLine());
-        //end of code used for initial debugging
-
-        ConnectionHandler connectionHandler;
-        try {
-            connectionHandler = new ConnectionHandler("localhost", socketPort);
-        } catch (IOException e){
-            System.out.println("Can't reach the server!");
-            return;
-        }
-        new Thread(connectionHandler).start();
-*/
 
 
