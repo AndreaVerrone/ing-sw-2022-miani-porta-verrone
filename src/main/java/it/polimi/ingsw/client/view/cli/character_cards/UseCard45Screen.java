41af0adf1731fe3bf5186c5e1ae0ff7ae242fbf3
AndreaVerrone <andrea.verrone@mail.polimi.it>
2022-06-27 16:32:20 +0200

    Modify the screen to use character card 4 and 5

--- src/main/java/it/polimi/ingsw/client/view/cli/character_cards/UseCard45Screen.java
+++ src/main/java/it/polimi/ingsw/client/view/cli/character_cards/UseCard45Screen.java
@@ -6,21 +6,22 @@ import it.polimi.ingsw.client.view.cli.CliScreen;
 import it.polimi.ingsw.client.view.cli.fancy_cli.inputs.InputReader;
 import it.polimi.ingsw.server.controller.game.Location;
 import it.polimi.ingsw.server.controller.game.Position;
+import it.polimi.ingsw.server.controller.game.expert.CharacterCardsType;
 import org.jline.reader.impl.completer.StringsCompleter;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
 /**
- *  A screen to use character card 4
+ *  A screen to use character card 4 or 5
  */
-public class UseCard4Screen extends CliScreen {
+public class UseCard45Screen extends CliScreen {
 
     /**
      * Creates a new screen used to use character card 4
      * @param cli the cli of the client
      */
-    public UseCard4Screen(CLI cli) {
+    public UseCard45Screen(CLI cli) {
         super(cli);
     }
 
